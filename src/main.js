498acebbe2d1f3e451eaa57a895c7b9faabb40de
psypow <psypow.dev@gmail.com>
2015-09-20 17:00:57 +0200

    naming and MIT license in package.json

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import angular from 'angular';
 import angularRoute from 'angular-route';
-import masteries from 'angular-lol-masteries';
-import masteriesTemplate from './masteries.tpl.html';
-
+import masteriesModule from 'angular-lol-masteries';
+import masteriesViewTemplate from './masteries.tpl.html';
 angular.module('angular-lol-showcase', [
     'ng',
     angularRoute,
-    masteries.name
+    masteriesModule.name
 ]).config(($routeProvider) => {
-    $routeProvider.otherwise({redirectTo:'/masteries'});
+    $routeProvider.otherwise({redirectTo: '/masteries'});
 
     $routeProvider.when('/masteries', {
-        template:masteriesTemplate,
-        controller:function(masteriesData){
+        template: masteriesViewTemplate,
+        controller: function (masteriesData) {
 
         }
     });
